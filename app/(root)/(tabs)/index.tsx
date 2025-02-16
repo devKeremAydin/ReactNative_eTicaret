3d8594687bb0f650c9e7d7a65a1b57988b2b70c9
kerem aydın <krmydn7@gmail.com>
2025-02-16 20:38:23 +0300

    Root function added

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-
+import {Link} from 'expo-router';
 export default function Index() {
   return (
     <View
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Kerem</Text>
+      <Link href="/cart">sepet</Link>
+      <Link href="/profile">profile</Link>
+      <Link href="/products">ürünler</Link>
     </View>
   );
 }
